99e0687b1e81d2ec9d2150ade44db07f74932863
nahal <mrym.mhdzde@gmail.com>
2024-11-15 09:28:14 +0330

    create static genesis method instance of Block

--- block.js
+++ block.js
@@ -1,9 +1,16 @@
+const { GENESIS_DATA } = require("./config");
+
 class Block {
   constructor({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  //factory method
+  static genesis() {
+    return new this(GENESIS_DATA);
   }
 }
 
