d20ccef430f4d8997aafeb6e238900c6d816d095
nahal <mrym.mhdzde@gmail.com>
2024-11-17 13:49:20 +0330

    post api for addBlock

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const Blockchain = require("./blockchain");
 
 const app = express();
+app.use(express.json());
 
 const blockchain = new Blockchain();
 
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+  res.redirect("/api/blocks");
 });
 
 const PORT = 3000;
