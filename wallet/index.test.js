9cc49843042094185f57f42a20696fab3ffde9d6
nahal <mrym.mhdzde@gmail.com>
2024-11-19 09:19:15 +0330

    each wallet can create transaction

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require("./index");
 const { verifySignature } = require("./../util");
+const Transaction = require("./transaction");
 
 describe("Wallet", () => {
   let wallet;
@@ -30,9 +31,37 @@ describe("Wallet", () => {
         verifySignature({
           publicKey: wallet.publicKey,
           data,
-          signature:new Wallet().sign(data),
+          signature: new Wallet().sign(data),
         })
       ).toBe(false);
+    });
+  });
+  describe("createTransaction()", () => {
+    describe("and the amount exceeds the balance", () => {
+      it("throws an error", () => {
+        expect(() =>
+          wallet.createTransaction({ amount: 9999, recipient: "foo-recipient" })
+        ).toThrow("amount exceeds balance");
+      });
+    });
+    describe("and the amount is valid", () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        recipient = "foo-recipient";
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+
+      it("creates an instance of `transaction`", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+      it("matches the transaction input with the wallet", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+      it("outputs the amount the recipient", () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
